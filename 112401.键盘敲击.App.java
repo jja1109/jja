9b3efaf6a68d17762f77d3e5f54b61d06ea92302
jja1109 <74757503+jja1109@users.noreply.github.com>
2020-11-24 20:32:48 +0800

    Create 112401.键盘敲击.App.java

---
+++ 112401.键盘敲击.App.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+
+public class App {
+    JPanel myPanel=new JPanel();
+    JTextArea textArea_A=new JTextArea();
+    JTextArea textArea_B=new JTextArea();
+
+    public App() {
+
+        textArea_A.setBounds(100, 100, 256, 128);
+        textArea_A.setBackground(Color.green);
+        myPanel.add(textArea_A);
+        textArea_B.setBounds(100, 300, 256, 128);
+        textArea_B.setBackground(Color.yellow);
+        myPanel.add(textArea_B);
+
+        textArea_A.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                super.keyPressed(e);
+                if (e.getKeyCode() == KeyEvent.VK_UP) {
+                    textArea_B.append("点击进入我的世界！\n");
+                }
+                if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                    textArea_B.setText("");
+                }
+            }
+        });
+    }
+    void go(){
+        JFrame frame = new JFrame("窗体");
+        myPanel.setLayout(null);
+        frame.setContentPane(myPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(100,100,512,720);
+        //frame.pack();
+        frame.setVisible(true);
+    }
+    public static void main(String[] args) {
+        new App().go();
+    }
+}
